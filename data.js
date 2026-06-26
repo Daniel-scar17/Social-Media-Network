8a4f587a445ba708464ed0e3394161963370f81b
Daniel-scar17 <daniellago4321@gmail.com>
2026-06-26 08:28:05 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,30 +1,105 @@
 export const defaultSocialData = {
-    April:{
-        Facebook:["Arianne", "Craig", "Daniel", "Kelly", "Viel"],
-        TikTok:["Daniel", "Kelly"],
-        X:["Craig"]
+    "Regine": {
+        "Facebook": ["April", "Craig", "Cresa", "Elmie", "Jenelyn", "Josefl", "Kelly Grace", "Kyrl", "Nylvia", "Phoeboy", "Row", "Steven", "Viel Son"]
     },
-    Arianne:{
-        Facebook:["April"],
-        Instagram:["Daniel"],
-        TikTok:["Viel"]
+    "April": {
+        "Facebook": ["Regine", "Ariane", "Craig", "Daniel", "Kelly"],
+        "Instagram": ["Ariane", "Daniel"],
+        "TikTok": ["Kelly"]
     },
-    Craig:{
-        Facebook:["April"],
-        X:["April", "Kelly"]
+    "Daniel": {
+        "Facebook": ["April", "Nylvia"],
+        "Instagram": ["April"],
+        "TikTok": ["Kelly", "Viel"]
     },
-    Daniel:{
-        Facebook:["April"],
-        Instagram:["Arianne"],
-        TikTok:["April", "Viel"]
+    "Ariane": {
+        "Facebook": ["April"],
+        "Instagram": ["Daniel"]
     },
-    Kelly:{
-        Facebook:["April"],
-        TikTok:["April"],
-        X:["Craig"]
+    "Craig": {
+        "Facebook": ["April", "Regine"]
     },
-    Viel:{
-        Facebook:["April"],
-        TikTok:["Daniel", "Arianne"]
+    "Kelly": {
+        "Facebook": ["April", "Nylvia"],
+        "TikTok": ["Daniel"]
+    },
+    "Viel": {
+        "Facebook": ["Mark"],
+        "TikTok": ["Daniel", "Vince"]
+    },
+    "Adrian": {
+        "Facebook": ["Dunavan", "Juhan", "Bin"]
+    },
+    "Althea": {
+        "Facebook": ["Nylvia"]
+    },
+    "Bin": {
+        "Facebook": ["Adrian", "James", "Johan", "Mark James"]
+    },
+    "Cresa": {
+        "Facebook": ["Regine", "Ivan"]
+    },
+    "Dunavan": {
+        "Facebook": ["Adrian"]
+    },
+    "Elmie": {
+        "Facebook": ["Regine"]
+    },
+    "Gian": {
+        "Facebook": ["Josefl"]
+    },
+    "Ivan": {
+        "Facebook": ["Cresa"]
+    },
+    "James": {
+        "Facebook": ["Bin"]
+    },
+    "Jenelyn": {
+        "Facebook": ["Regine", "Joshua"]
+    },
+    "Josefl": {
+        "Facebook": ["Regine", "Gian", "Mark"]
+    },
+    "Johan": {
+        "Facebook": ["Bin"]
+    },
+    "Joshua": {
+        "Facebook": ["Jenelyn"]
+    },
+    "Juhan": {
+        "Facebook": ["Adrian"]
+    },
+    "Kelly Grace": {
+        "Facebook": ["Regine"]
+    },
+    "Kyrl": {
+        "Facebook": ["Regine"]
+    },
+    "Mark James": {
+        "Facebook": ["Bin"]
+    },
+    "Mark": {
+        "Facebook": ["Josefl", "Viel"]
+    },
+    "Nylvia": {
+        "Facebook": ["Regine", "Daniel", "Kelly", "Althea"]
+    },
+    "Phoeboy": {
+        "Facebook": ["Regine"]
+    },
+    "Row": {
+        "Facebook": ["Regine"]
+    },
+    "Steven": {
+        "Facebook": ["Regine"]
+    },
+    "Viel Son": {
+        "Facebook": ["Regine"]
+    },
+    "Vince": {
+        "Facebook": ["Viel"]
+    },
+    "Zeeshan": {
+        "Facebook": []
     }
 };
